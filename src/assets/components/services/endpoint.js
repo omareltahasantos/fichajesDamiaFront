6edb36f03c7f160cfc036346ab673d9ae924828a
Omar El Taha Santos <omareltahasan6@gmail.com>
2024-02-07 13:48:53 +0100

    SORT COLUMNS IN HOURS TABEL

--- src/assets/components/services/endpoint.js
+++ src/assets/components/services/endpoint.js
@@ -1,3 +1,3 @@
-Clet endpoint = 'https://smfichajes.herokuapp.com/api/'
+let endpoint = 'https://smfichajes.herokuapp.com/api/'
 //let endpoint = 'http://localhost:8000/api/'
 export default endpoint
