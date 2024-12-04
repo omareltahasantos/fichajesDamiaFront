88ce962ec0e580b54280d2369bbf2f1c0c37a66d
Omar El Taha Santos <omareltahasan6@gmail.com>
2024-12-04 15:35:55 +0100

    adad

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom' // Not 'react-dom/client' in React 17
+import App from './App'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
+// This is how you render the app in React 17
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+)
