4904dec6cf4151999da143ba90e63122acacde9a
Omar El Taha Santos <omareltahasan6@gmail.com>
2025-05-12 12:25:32 +0200

    adad

--- src/assets/components/services/endpoint.js
+++ src/assets/components/services/endpoint.js
@@ -1,4 +1,4 @@
-//let endpoint = 'https://smfichajes.herokuapp.com/api/'
+let endpoint = 'https://smfichajes.herokuapp.com/api/'
 //let endpoint = 'http://localhost:8000/api/'
-let endpoint = 'http://localhost:8001/api/'
+//let endpoint = 'http://localhost:8001/api/'
 export default endpoint
